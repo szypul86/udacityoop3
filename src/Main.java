3d0c103581fbb60e844aada689dfebb632338163
Andrzej Szypulski <szypul86@gmail.com>
2019-06-25 21:24:16 +0200

    Scanner modificated to numbers input extra

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,18 @@ public class Main {
     public static void main(String [] args){
         System.out.println("Please type your address and submit with 'enter' key");
         Scanner scanner = new Scanner(System.in);
-        // nextline() - method that will return an input that user typed before hitting enter button
+        // nextline() - method that will return a string input input that user typed before hitting enter button
         String address = scanner.nextLine();
         System.out.println("Your address is: " + address);
+
+        System.out.println("now give me a numer:");
+        Scanner numberScanner = new Scanner(System.in);
+        int number = numberScanner.nextInt();
+        if(number == 69)
+            System.out.println("You nasty human!");
+        else
+            System.out.println("Your magic numer is " + number);
+
+
     }
 }
